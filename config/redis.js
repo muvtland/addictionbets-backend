18fd065e041be07142703ff530a8d912b7303074
Eduard Arustamyan <e.arustamyan@ongrid.pro>
2023-12-08 18:02:54 +0400

    add redis creds with env

--- config/redis.js
+++ config/redis.js
@@ -1,5 +1,17 @@
 import { createClient } from "redis";
+const redisPassword = process.env.REDIS_PASSWORD;
+const redisHost = process.env.REDIS_HOST;
+// export const client = await createClient()
+//     .on('error', err => console.log('Redis Client Error', err))
+//     .connect();
+// import { createClient } from 'redis';
 
-export const client = await createClient()
+export const client = await createClient({
+    password: redisPassword,
+    socket: {
+        host: redisHost,
+        port: 10806
+    }
+})
     .on('error', err => console.log('Redis Client Error', err))
     .connect();
